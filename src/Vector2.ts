2fa6328efaeae4ba98b542a515e536ec1581de22
ThomasGysemans <support@sciencesky.fr>
2024-02-28 12:11:10 +0100

    Add comments on Vector2 class

--- src/Vector2.ts
+++ src/Vector2.ts
@@ -1,3 +1,7 @@
+/**
+ * Represents coordinates within a 2D space.
+ * Can also be used to represent speed, movement and direction.
+ */
 export default class Vector2 {
   private _x: number = 0;
   private _y: number = 0;
