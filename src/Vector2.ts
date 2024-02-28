8d1ecc6f6cbfbca09f3075a6d81cb8c1c47343bc
Valentin Thuillier <valentin.thuillier@luxferre-code.fr>
2024-02-28 12:19:37 +0100

    [TDD] Refactoring code, test ok !

--- src/Vector2.ts
+++ src/Vector2.ts
@@ -3,9 +3,14 @@
  * Can also be used to represent speed, movement and direction.
  */
 export default class Vector2 {
-  private _x: number = 0;
-  private _y: number = 0;
+  private _x: number;
+  private _y: number;
 
+  /**
+   * Creates a new Vector2 instance.
+   * @param [x=0] - The x coordinate.
+   * @param [y=0] - The y coordinate
+   */
   constructor(x: number = 0, y: number = 0) {
     this._x = x;
     this._y = y;
