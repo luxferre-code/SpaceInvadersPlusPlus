954d7c1df3c8354786dfbda48e270d8489efbc6c
Valentin Thuillier <valentin.thuillier@luxferre-code.fr>
2024-02-28 12:17:32 +0100

    [TDD] Write test Vector2 constructor with parameters

--- src/tests/Vector2.test.ts
+++ src/tests/Vector2.test.ts
@@ -3,8 +3,13 @@ import Vector2 from "../Vector2";
 
 describe("Tests for Vector2 class", () => {
   test("should be instantiated with (0;0) by default", () => {
-    const vec2 = new Vector2();
+    const vec2: Vector2 = new Vector2();
     expect(vec2.x).toEqual(0);
     expect(vec2.y).toEqual(0);
   });
+  test('should be instantiated with parameter', () => {
+      const vector: Vector2 = new Vector2(1, 2);
+      expect(vector.x).toEqual(1);
+      expect(vector.y).toEqual(2);
+  })
 });
