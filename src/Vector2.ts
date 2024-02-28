bb8d82ce2bb3d61d879359b22b0c61e0677365db
Valentin Thuillier <valentin.thuillier@luxferre-code.fr>
2024-02-28 12:18:23 +0100

    [TDD] Code implemented

--- src/Vector2.ts
+++ src/Vector2.ts
@@ -6,6 +6,11 @@ export default class Vector2 {
   private _x: number = 0;
   private _y: number = 0;
 
+  constructor(x: number = 0, y: number = 0) {
+    this._x = x;
+    this._y = y;
+  }
+
   public get x() { return this._x; }
   public get y() { return this._y; }
 }
