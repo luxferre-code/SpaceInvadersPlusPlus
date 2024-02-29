7b9bd095ce56b86b655bb116d72e93d156e0a593
Valentin Thuillier <valentin.thuillier@luxferre-code.fr>
2024-02-29 11:56:20 +0100

    [TDD] Initialize Player.test.ts & test constructor (with mock to canvas & image)

---
+++ src/Player.ts
@@ -0,0 +1,53 @@
+import Vector2 from "./Vector2";
+
+/**
+ * Class representing a player
+ */
+export default class Player {
+
+    private static maxHP: number = 5;
+    private static speedAcceleration: number = 1;
+    private static maxSpeed: number = 20;
+    private horizontalMovement: boolean = false;
+    private verticalMovement: boolean = false;
+    private imageLoaded: boolean = false;
+    private _name: string;
+    private _score: number;
+    private _color: string;
+    private _hp: number;
+    private _position: Vector2;
+    private _speed: Vector2;
+    private _context: CanvasRenderingContext2D;
+    private _skin: HTMLImageElement;
+
+    constructor(name: string, color: string, canvas: HTMLCanvasElement, position: Vector2 = new Vector2(0, 0), disableControls: boolean = false, skin: HTMLImageElement = new Image()) {
+        this._name = name;
+        this._score = 0;
+        this._color = color;
+        this._hp = Player.maxHP;
+        this._position = position;
+        this._speed = new Vector2();
+        this._context = canvas.getContext("2d")!;
+        if(!disableControls) this.addEventListener(canvas);
+        this._skin = skin;
+        this._skin.src = "/assets/skins/skin-red.png";
+        this._skin.onload = () => this.imageLoaded = true;
+    }
+
+    private addEventListener(canvas: HTMLCanvasElement) : void {}
+
+    public get name() : string { return this._name; }
+    public set name(name: string) { this._name = name; }
+    public get score() : number { return this._score; }
+    public set score(score: number) { this._score = score; }
+    public get color() : string { return this._color; }
+    public set color(color: string) { this._color = color; }
+    public get hp() : number { return this._hp; }
+    public set hp(hp: number) { this._hp = hp; }
+    public get position() : Vector2 { return this._position; }
+    public set position(position: Vector2) { this._position = position; }
+    public get speed() : Vector2 { return this._speed; }
+    public set speed(speed: Vector2) { this._speed = speed; }
+
+
+}
---
+++ src/tests/Player.test.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from "vitest";
+import Player from '../Player';
+
+class CanvasMock {
+    getContext(context: string) {
+        return {
+            fillRect: () => {}
+        };
+    }
+}
+
+class ImageMock {
+    src: string = "";
+}
+
+describe("Tests for Player class", () => {
+    test('should be instantiated with default parameters', () => {
+        const player: Player = new Player("Player1", "red", new CanvasMock() as unknown as HTMLCanvasElement, undefined, false, new ImageMock() as unknown as HTMLImageElement);
+        expect(player.name).toEqual("Player1");
+        expect(player.score).toEqual(0);
+        expect(player.color).toEqual("red");
+        expect(player.hp).toEqual(5);
+        expect(player.position.x).toEqual(0);
+        expect(player.position.y).toEqual(0);
+        expect(player.speed.x).toEqual(0);
+        expect(player.speed.y).toEqual(0);
+    });
+});
