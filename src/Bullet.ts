01db9c6740d88b9d75470c37ed41d5168e833439
Valentin Thuillier <valentin.thuillier.etu@univ-lille.fr>
2024-03-06 11:21:37 +0100

    [TDD] Code implemented, no refactoring

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -5,7 +5,9 @@ export default class Bullet {
 
     private _position: Vector2;
     private _velocity: Vector2;
+    private _owner : any;
     public static _isVertical: boolean = true;
+    public static _bulletSpeed: number = 10;
 
     constructor(position: Vector2) {
         this._position = position;
@@ -14,9 +16,12 @@ export default class Bullet {
 
     public get position() : Vector2 { return this._position; }
     public get velocity() : Vector2 { return this._velocity; }
+    public get owner() : any { return this._owner; }
 
     public attachTo(player: Player) : void {
-        
+        this._owner = player;
+        this._position = player.position;
+        this._velocity = new Vector2(!Bullet._isVertical ? Bullet._bulletSpeed : 0, Bullet._isVertical ? -Bullet._bulletSpeed : 0);
     }
 
 }
