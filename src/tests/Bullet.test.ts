0a70038eb3cc6d1d6fd2c5fb09ee01d2fe3cabc6
Valentin Thuillier <valentin.thuillier.etu@univ-lille.fr>
2024-03-06 11:22:51 +0100

    [TDD] Test instance bullet without parameter

--- src/tests/Bullet.test.ts
+++ src/tests/Bullet.test.ts
@@ -15,6 +15,12 @@ describe('Test for Bullet Class', () => {
         expect(bullet.position).toEqual(new Vector2(0, 0));
         expect(bullet.velocity).toEqual(new Vector2(0, 0));
     });
+    test('should be instanciated with 0 parameters', () => {
+        const bullet: Bullet = new Bullet();
+        expect(bullet).toBeDefined();
+        expect(bullet.position).toEqual(new Vector2(0, 0));
+        expect(bullet.velocity).toEqual(new Vector2(0, 0));
+    });
     test('should by attached to a player', () => {
         const bullet: Bullet = new Bullet(new Vector2(-1, -1));
         const player: Player = createDummyPlayer(canvas, img);
