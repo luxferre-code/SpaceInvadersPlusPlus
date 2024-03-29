ab78c21226d0fb6725435b545bfde8cc44a4efa4
Valentin THUILLIER <valentin.thuilliere.etu@univ-lille.fr>
2024-03-29 10:27:00 +0100

    Initialize server config

---
+++ server/server.ts
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import express from "express";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  // ...
+});
+
+app.use(express.static('../public', {
+  index: "express/index.html"
+}));
+
+httpServer.listen(port, () => {
+  console.log("listening on port " + port);
+});
